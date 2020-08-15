c4ac08cbcec37d1535deb52a43a2f316a8beb8e4
Brunno Vodola <bvodola@gmail.com>
2020-08-15 00:57:01 -0300

    Changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { TinaProvider, TinaCMS } from "tinacms";
-import "../styles/globals.css";
 
-const cms = new TinaCMS({ enabled: true, sidebar: true });
+const cms = new TinaCMS({ enabled: true, toolbar: true });
 
 const App = (props) => {
   const { Component, pageProps } = props;
 
-  // Wrap the TinaProvider around all page components
   return (
     <TinaProvider cms={cms}>
       <Component {...pageProps} />
